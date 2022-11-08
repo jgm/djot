836a8b80ac58a9cfb0e5fe06e0eb2a8a1bb6289d
John MacFarlane <jgm@berkeley.edu>
2022-11-08 09:15:59 -0800

    clib: test suite.

---
+++ clib/tests.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include "djot.h"
+
+int failed = 0;
+int num = 0;
+
+static void asserteq(int actual, int expected) {
+  num = num + 1;
+  if (actual == expected) {
+    printf("Test %4d PASSED\n", num);
+  } else {
+    printf("Test %4d FAILED\nExpected %d, got %d\n", num, expected, actual);
+    failed = failed + 1;
+  }
+}
+
+static int error(lua_State *L) {
+  djot_report_error(L);
+  return -1;
+}
+
+int main (void) {
+  char *out;
+
+  /* Do this once, before any use of the djot library */
+  lua_State *L = djot_open();
+  if (!L) error(L);
+
+  /* Now use functions like djot_to_json_ast */
+  out = djot_to_json_ast(L, "hi *there*\n");
+  if (!L) error(L);
+
+  /* Note: we just compare lengths, because JSON rendering is
+   * non-deterministic. */
+  asserteq(strlen(out), strlen("{\"children\":[{\"children\":[{\"text\":\"hi \",\"tag\":\"str\"},{\"children\":[{\"text\":\"there\",\"tag\":\"str\"}],\"tag\":\"strong\"}],\"tag\":\"para\"}],\"tag\":\"doc\",\"references\":[],\"footnotes\":[]}\n"));
+
+  /* When you're finished, close the djot library */
+  djot_close(L);
+
+  /* Check that the string returned is still available
+   * after closing the lua state: */
+  asserteq(strlen(out), strlen("{\"children\":[{\"children\":[{\"text\":\"hi \",\"tag\":\"str\"},{\"children\":[{\"text\":\"there\",\"tag\":\"str\"}],\"tag\":\"strong\"}],\"tag\":\"para\"}],\"tag\":\"doc\",\"references\":[],\"footnotes\":[]}\n"));
+
+  if (failed) {
+    printf("%d tests failed.\n", failed);
+  } else {
+    printf("All good.\n");
+  }
+  return failed;
+}
