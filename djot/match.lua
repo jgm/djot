ca418fc9dd5ea02c7dc6d39191da054f0e13ac78
John MacFarlane <jgm@berkeley.edu>
2022-11-13 14:42:38 -0800

    Don't use string.pack for matches.
    It saves a bit of memory but performance is actually worse.

--- djot/match.lua
+++ djot/match.lua
@@ -1,29 +1,10 @@
-local make_match, unpack_match
 local unpack = unpack or table.unpack
 
-if jit or not string.pack then
-  -- for luajit or lua 5.1, we don't have string.pack/unpack, so we use arrays.
-  -- This is faster than using ffi to pack things in C structs.
-
-  make_match = function(startpos, endpos, annotation)
-    return {startpos, endpos, annotation}
-  end
-
-  unpack_match = unpack
-
-else
-  -- for standard lua >= 5.2, we use string.pack/unpack which gives a
-  -- more memory-efficient representation than arrays.
-
-  make_match = function(startpos, endpos, annotation)
-    return string.pack("=I4I4z", startpos, endpos, annotation)
-  end
-
-  unpack_match = function(match)
-    local startpos, endpos, annotation = string.unpack("=I4I4z", match)
-    return startpos, endpos, annotation
-  end
+local make_match = function(startpos, endpos, annotation)
+  return {startpos, endpos, annotation}
 end
+
+local unpack_match = unpack
 
 local get_length = function(match)
   local startpos, endpos = unpack_match(match)
