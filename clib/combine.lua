7850bad101587e913c3dc14613cb97a36c449cd3
John MacFarlane <jgm@berkeley.edu>
2022-11-30 19:39:31 -0800

    Minify lua before including in C source.
    This cuts down the size of djot.o considerably.

--- clib/combine.lua
+++ clib/combine.lua
@@ -1,4 +1,6 @@
 -- combine modules (specified as cli arguments) into one file
+package.path = "./?.lua;" .. package.path
+local parser = require("dumbParser")
 
 local modules = {}
 for i=1,#arg do
@@ -6,14 +8,24 @@ for i=1,#arg do
     arg[i]:gsub("^../",""):gsub("%.lua$",""):gsub("%/",".")
 end
 
+local buffer = {}
+local function out(s)
+  buffer[#buffer + 1] = s
+end
+
 for _,module in ipairs(modules) do
-  print(string.format('package.preload["%s"] = function()', module))
+  out(string.format('package.preload["%s"] = function()', module))
   local path = "../" .. module:gsub("%.","/") .. ".lua"
   local f = assert(io.open(path, "r"))
   local content = f:read("*all")
-  print(content)
-  print('end\n')
+  out(content)
+  out('end\n')
 end
 
-print('local djot = require("djot")')
-print('return djot')
+out('local djot = require("djot")')
+out('return djot')
+
+local combined = table.concat(buffer, "\n")
+local ast = parser.parse(combined)
+parser.minify(ast)
+io.stdout:write(parser.toLua(ast, false))
