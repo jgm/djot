73113ec7f43da141aa6bcb34195e8eb570212ce9
John MacFarlane <jgm@berkeley.edu>
2026-02-07 11:14:39 +0100

    Use the JavaScript reference implementation, not the Lua one...
    to generate expected output in syntax.html.

--- doc/code-examples.lua
+++ doc/code-examples.lua
@@ -1,11 +1,19 @@
-local djot = require("djot")
+function pipe_through(command, input)
+    local tmp = os.tmpname()
+    local tmpfile = io.open(tmp, "w")
+    tmpfile:write(input)
+    tmpfile:close()
 
-function renderdjot(txt)
-  return djot.render_html(djot.parse(txt))
+    local handle = io.popen(command .. " < " .. tmp, "r")
+    local output = handle:read("*a")
+    handle:close()
+
+    os.remove(tmp)
+    return output
 end
 
 function CodeBlock(el)
-  local rendered = renderdjot(el.text)
+  local rendered = pipe_through('djot', el.text)
   return {
     pandoc.Div(
     { pandoc.Div({pandoc.CodeBlock(el.text)}, {class="djot"}),
